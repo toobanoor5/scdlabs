19398e87ebe73e386d1b3deb3c5a4b73b463bd89
toobanoor5 <toobanoor320gb@gmail.com>
2025-10-23 12:26:12 +0500

    Again commit

--- lab2/src/lab2/task1.java
+++ lab2/src/lab2/task1.java
@@ -5,12 +5,15 @@ class task1 {
 	public static void main(String[] args) {
 		int a =10;
 		if(a>0) {
-			System.out.println("Number is positive");
+			System.out.println("Number is Positive");
 		}
-		else {
-			System.out.println("Number is negative");
+		else if(a<0) {
+			System.out.println("Number is Negative");
 	
 		}
+		else{
+						System.out.println("Number is Zero");
+
 	}
 
 }
